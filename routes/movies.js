c94c5a6bb13d7fedb2caf630ff5d70a92ea86179
gmorklla <gmorklla@gmail.com>
2023-12-19 18:25:40 -0600

    Chat - Webhooks - Update: movies route refactor

--- routes/movies.js
+++ routes/movies.js
@@ -16,46 +16,46 @@ router.all("/*", (req, res, next) => {
 });
 
 router.post("/", (req, res) => {
-  const movieToSearch =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters["movie-name"]
-      ? req.body.queryResult.parameters["movie-name"]
-      : "The Godfather";
-  const reqUrl = encodeURI(
+  const {
+    body: { queryResult: { parameters = {} } = {} },
+  } = req;
+  const movieToSearch = parameters["movie-name"] || "The Godfather";
+  const urlAPI = encodeURI(
     `http://www.omdbapi.com/?t=${movieToSearch}&apikey=${MOVIE_API_KEY}`
   );
   http.get(
-    reqUrl,
-    (responseFromAPI) => {
+    urlAPI,
+    (resAPI) => {
       let completeResponse = "";
-      responseFromAPI.on("data", (chunk) => {
+      resAPI.on("data", (chunk) => {
         completeResponse += chunk;
       });
-      responseFromAPI.on("end", () => {
-        const movie = JSON.parse(completeResponse);
-        let dataToSend =
-          movieToSearch === "The Godfather"
-            ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n`
-            : "";
-        dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
-        const agent = new WebhookClient({ request: req, response: res });
-        function movieData(agent) {
-          agent.add(dataToSend);
-        }
-        let intentMap = new Map();
-        intentMap.set("movie-intent", movieData);
-        agent.handleRequest(intentMap);
-      });
+      resAPI.on("end", () => endCallback(req, res, completeResponse, movieToSearch));
     },
     (error) => {
       return res.json({
-        speech: "Something went wrong!",
-        displayText: "Something went wrong!",
+        speech: "¡Algo salió mal!",
+        displayText: "¡Algo salió mal!",
         source: "get-movie-details",
       });
     }
   );
 });
+
+function endCallback(req, res, completeResponse, movieToSearch) {
+  const movie = JSON.parse(completeResponse);
+  let dataToSend =
+    movieToSearch === "The Godfather"
+      ? `No tengo la información al respecto, pero aquí tienes algunos datos sobre 'The Godfather'.\n`
+      : "";
+  dataToSend += `${movie.Title} es una película de ${movie.Genre} protagonizada por ${movie.Actors}, estrenada en ${movie.Year} y dirigida por ${movie.Director}.`;
+  const agent = new WebhookClient({ request: req, response: res });
+  function movieData(agent) {
+    agent.add(dataToSend);
+  }
+  let intentMap = new Map();
+  intentMap.set("movie-intent", movieData);
+  agent.handleRequest(intentMap);
+}
 
 module.exports = router;
