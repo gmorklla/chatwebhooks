2d00c18d0f007fc03b0fb801a7d0949ddb532d94
gmorklla <gmorklla@gmail.com>
2023-12-19 21:06:16 -0600

    Chat - Webhooks - Update: intent transfer con rich response card

--- intents/transfer.js
+++ intents/transfer.js
@@ -1,14 +1,44 @@
-const { WebhookClient } = require("dialogflow-fulfillment");
+const { WebhookClient, Card } = require("dialogflow-fulfillment");
+
+const intentMap = new Map();
+intentMap.set("transferIntent", transferData);
+
+function transferData(agent) {
+  const card = new Card({
+    title: "card title",
+    text: "card text",
+    imageUrl:
+      "https://b00c-2806-2f0-9181-e855-69b4-4d68-e16-5638.ngrok-free.app/images/app-development.jpg",
+    buttonText: "This is a button",
+    buttonUrl: "https://assistant.google.com/"
+  });
+  const card2 = new Card({
+    title: "card title 2",
+    text: "card text 2",
+    imageUrl:
+      "https://b00c-2806-2f0-9181-e855-69b4-4d68-e16-5638.ngrok-free.app/images/app-development.jpg",
+    buttonText: "This is a button 2",
+    buttonUrl: "https://assistant.google.com/"
+  });
+  agent.add([card, card2]);
+}
+
+const errorIntentMap = new Map();
+errorIntentMap.set("transferIntent", errorData);
+
+function errorData(agent) {
+  const dataToSend = "Error!!!";
+  agent.add(dataToSend);
+}
 
 function response(req, res) {
-  const dataToSend = 'Transfer intent response from Node JS';
   const agent = new WebhookClient({ request: req, response: res });
-  function transferData(agent) {
-    agent.add(dataToSend);
+  try {
+    agent.handleRequest(intentMap);
+  } catch (error) {
+    console.log("ERROR", error);
+    agent.handleRequest(errorIntentMap);
   }
-  let intentMap = new Map();
-  intentMap.set("transferIntent", transferData);
-  agent.handleRequest(intentMap);
 }
 
 module.exports = response;
